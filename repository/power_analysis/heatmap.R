07d683193662045274f46faba1d534a4baf2e6b7
Tiago Ribeiro <ribeirotgs@gmail.com>
2021-07-20 12:14:52 -0500

    Update plot settings.

--- repository/power_analysis/heatmap.R
+++ repository/power_analysis/heatmap.R
@@ -11,11 +11,20 @@ library(ggplot2)
 
 setwd('/media/tiago/Seagate Portable Drive/power_analysis/power_results_10k/')
 
-heatmap_power_gg <- function(dset, varX, varY, varFill, varTitle){
-  gg_inc_fst_ss <- ggplot(dset, aes_string(varX, varY, fill=varFill)) + geom_tile() + scale_fill_viridis_c(option="B", direction=-1) + 
-    theme_light() + ggtitle(varTitle) + geom_text(aes_string(label=varFill))
-  return(gg_inc_fst_ss)
+heatmap_power_gg <- function(dset, varX, varY, varFill, varTitle, xLabel, yLabel, varFigname){
+  dset[,3] <- round(dset[,3], digits = 2)
+  gg_heatmap <- ggplot(dset, aes_string(varX, varY, fill=varFill)) + geom_tile() + scale_fill_viridis_c(option="cividis", direction=-1, name="Power") + 
+    theme_light() + ggtitle(varTitle) + theme(axis.text = element_text(size = 15)) + theme(axis.title = element_text(size = 25)) +
+    theme(plot.title = element_text(size=22)) + xlab(xLabel) + ylab(yLabel) + 
+    geom_text(aes_string(label=varFill, size=2, colour=shQuote("white"), fontface = shQuote('bold')), show.legend=F)
+  
+  figname = varFigname
+  ggsave(filename=figname, plot = gg_heatmap, width = 10, height = 8, dpi = 150, units = "in", device='jpeg')
+  return(gg_heatmap)
 }
+
+gg_inc_fst_ss <- heatmap_power_gg(inc_fst_ss, 'init_Freq', 'final_Freq', 'power', plot_title, "Starting Frequency", "Ending Frequency", plot_name)
+gg_inc_fst_ss
 
 complete_fst <- read.csv('complete_fst.txt', sep='\t')[,1:3] # ModelID, SNP_FST, winFST
 incomplete_fst <- read.csv('incomplete_fst.txt', sep='\t')[,1:3] # ModelID, SNP_FST, winFST
@@ -49,8 +58,9 @@ for (i in 1:length(incomplete_fst_ss$ModelID)){
 inc_fst_ss <- data.frame(inc_fst_ss)
 names(inc_fst_ss) <- c('init_Freq', 'final_Freq', 'power')
 inc_fst_ss$power <- as.numeric(inc_fst_ss$power)
-plot_title = 'SNP FST - Incomplete Sweep - HN (s=0.01)'
-gg_inc_fst_ss <- heatmap_power_gg(inc_fst_ss, 'init_Freq', 'final_Freq', 'power', plot_title)
+plot_title = expression(italic(F[ST - MaxSNP])~" - Incomplete Sweep - High"~ italic(N[e])~" (s=0.01)")
+plot_name = 'inc_hn_ss_snp.jpg'
+gg_inc_fst_ss <- heatmap_power_gg(inc_fst_ss, 'init_Freq', 'final_Freq', 'power', plot_title, "Starting Frequency", "Ending Frequency", plot_name)
 gg_inc_fst_ss
 
 ### Window FST
@@ -65,8 +75,9 @@ for (i in 1:length(incomplete_fst_ss$ModelID)){
 inc_fst_ss_win <- data.frame(inc_fst_ss_win)
 names(inc_fst_ss_win) <- c('init_Freq', 'final_Freq', 'power')
 inc_fst_ss_win$power <- as.numeric(inc_fst_ss_win$power)
-plot_title = 'Window FST - Incomplete Sweep - HN (s=0.01)'
-gg_inc_fst_ss_win <- heatmap_power_gg(inc_fst_ss_win, 'init_Freq', 'final_Freq', 'power', plot_title)
+plot_title = expression(italic(F[ST - Window])~" - Incomplete Sweep - High"~ italic(N[e])~" (s=0.01)")
+plot_name = 'inc_hn_ss_win.jpg'
+gg_inc_fst_ss_win <- heatmap_power_gg(inc_fst_ss_win, 'init_Freq', 'final_Freq', 'power', plot_title, "Starting Frequency", "Ending Frequency", plot_name)
 gg_inc_fst_ss_win
 
 ## Chi
@@ -81,8 +92,10 @@ for (i in 1:length(incomplete_chi_ss$ModelID)){
 inc_chi_ss <- data.frame(inc_chi_ss)
 names(inc_chi_ss) <- c('init_Freq', 'final_Freq', 'power')
 inc_chi_ss$power <- as.numeric(inc_chi_ss$power)
-plot_title = 'CHI - Incomplete Sweep - HN (s=0.01)'
-gg_inc_chi_ss <- heatmap_power_gg(inc_chi_ss, 'init_Freq', 'final_Freq', 'power', plot_title)
+#plot_title = 'CHI - Incomplete Sweep - HN (s=0.01)'
+plot_title = expression(italic("\u03A7"[MD])~" - Incomple Sweep - High"~ italic(N[e])~" (s=0.01)")
+plot_name = 'inc_hn_ss_chi.jpg'
+gg_inc_chi_ss <- heatmap_power_gg(inc_chi_ss, 'init_Freq', 'final_Freq', 'power', plot_title, "Starting Frequency", "Ending Frequency", plot_name)
 gg_inc_chi_ss
 
 
@@ -98,8 +111,9 @@ for (i in 1:length(incomplete_fst_ws$ModelID)){
 inc_fst_ws <- data.frame(inc_fst_ws)
 names(inc_fst_ws) <- c('init_Freq', 'final_Freq', 'power')
 inc_fst_ws$power <- as.numeric(inc_fst_ws$power)
-plot_title = 'SNP FST - Incomplete Sweep - HN (s=0.001)'
-gg_inc_fst_ws <- heatmap_power_gg(inc_fst_ws, 'init_Freq', 'final_Freq', 'power', plot_title)
+plot_title = expression(italic(F[ST - MaxSNP])~" - Incomple Sweep - High"~ italic(N[e])~" (s=0.001)")
+plot_name = 'inc_hn_ws_snp.jpg'
+gg_inc_fst_ws <- heatmap_power_gg(inc_fst_ws, 'init_Freq', 'final_Freq', 'power', plot_title, "Starting Frequency", "Ending Frequency", plot_name)
 gg_inc_fst_ws
 
 ###### Window
@@ -114,8 +128,9 @@ for (i in 1:length(incomplete_fst_ws$ModelID)){
 inc_fst_ws_win <- data.frame(inc_fst_ws_win)
 names(inc_fst_ws_win) <- c('init_Freq', 'final_Freq', 'power')
 inc_fst_ws_win$power <- as.numeric(inc_fst_ws_win$power)
-plot_title = 'Window FST - Incomplete Sweep - HN (s=0.001)'
-gg_inc_fst_ws_win <- heatmap_power_gg(inc_fst_ws_win, 'init_Freq', 'final_Freq', 'power', plot_title)
+plot_title = expression(italic(F[ST - Window])~" - Incomple Sweep - High"~ italic(N[e])~" (s=0.001)")
+plot_name = 'inc_hn_ws_win.jpg'
+gg_inc_fst_ws_win <- heatmap_power_gg(inc_fst_ws_win, 'init_Freq', 'final_Freq', 'power', plot_title, "Starting Frequency", "Ending Frequency", plot_name)
 gg_inc_fst_ws_win
 
 # CHI
@@ -130,8 +145,9 @@ for (i in 1:length(incomplete_chi_ws$ModelID)){
 inc_chi_ws <- data.frame(inc_chi_ws)
 names(inc_chi_ws) <- c('init_Freq', 'final_Freq', 'power')
 inc_chi_ws$power <- as.numeric(inc_chi_ws$power)
-plot_title = 'CHI - Incomplete Sweep - HN (s=0.001)'
-gg_inc_chi_ws <- heatmap_power_gg(inc_chi_ws, 'init_Freq', 'final_Freq', 'power', plot_title)
+plot_title = expression(italic("\u03A7"[MD])~" - Incomple Sweep - High"~ italic(N[e])~" (s=0.001)")
+plot_name = 'inc_hn_ws_chi.jpg'
+gg_inc_chi_ws <- heatmap_power_gg(inc_chi_ws, 'init_Freq', 'final_Freq', 'power', plot_title, "Starting Frequency", "Ending Frequency", plot_name)
 gg_inc_chi_ws
 
 
@@ -148,8 +164,9 @@ for (i in 1:length(incomplete_fst_LN$ModelID)){
 inc_fst_ln <- data.frame(inc_fst_ln)
 names(inc_fst_ln) <- c('init_Freq', 'final_Freq', 'power')
 inc_fst_ln$power <- as.numeric(inc_fst_ln$power)
-plot_title = 'SNP FST - Incomplete Sweep - LN (s=0.01)'
-gg_inc_fst_ln <- heatmap_power_gg(inc_fst_ln, 'init_Freq', 'final_Freq', 'power', plot_title)
+plot_title = expression(italic(F[ST - MaxSNP])~" - Incomple Sweep - Low"~ italic(N[e])~" (s=0.01)")
+plot_name = 'inc_ln_snp.jpg'
+gg_inc_fst_ln <- heatmap_power_gg(inc_fst_ln, 'init_Freq', 'final_Freq', 'power', plot_title, "Starting Frequency", "Ending Frequency", plot_name)
 gg_inc_fst_ln
 
 
@@ -166,8 +183,9 @@ for (i in 1:length(incomplete_fst_LN$ModelID)){
 inc_fst_ln_win <- data.frame(inc_fst_ln_win)
 names(inc_fst_ln_win) <- c('init_Freq', 'final_Freq', 'power')
 inc_fst_ln_win$power <- as.numeric(inc_fst_ln_win$power)
-plot_title = 'Window FST - Incomplete Sweep - LN (s=0.01)'
-gg_inc_fst_ln_win <- heatmap_power_gg(inc_fst_ln_win, 'init_Freq', 'final_Freq', 'power', plot_title)
+plot_title = expression(italic(F[ST - Window])~" - Incomple Sweep - Low"~ italic(N[e])~" (s=0.01)")
+plot_name = 'inc_ln_win.jpg'
+gg_inc_fst_ln_win <- heatmap_power_gg(inc_fst_ln_win, 'init_Freq', 'final_Freq', 'power', plot_title, "Starting Frequency", "Ending Frequency", plot_name)
 gg_inc_fst_ln_win
 
 # CHI
@@ -182,8 +200,9 @@ for (i in 1:length(incomplete_chi_LN$ModelID)){
 inc_chi_ln <- data.frame(inc_chi_ln)
 names(inc_chi_ln) <- c('init_Freq', 'final_Freq', 'power')
 inc_chi_ln$power <- as.numeric(inc_chi_ln$power)
-plot_title = 'CHI - Incomplete Sweep - LN (s=0.01)'
-gg_inc_chi_ln <- heatmap_power_gg(inc_chi_ln, 'init_Freq', 'final_Freq', 'power', plot_title)
+plot_title = expression(italic("\u03A7"[MD])~" - Incomple Sweep - Low"~ italic(N[e])~" (s=0.01)")
+plot_name = 'inc_ln_chi.jpg'
+gg_inc_chi_ln <- heatmap_power_gg(inc_chi_ln, 'init_Freq', 'final_Freq', 'power', plot_title, "Starting Frequency", "Ending Frequency", plot_name)
 gg_inc_chi_ln
 
 
@@ -219,8 +238,9 @@ migr_fst_ss <- data.frame(migr_fst_ss)
 names(migr_fst_ss) <- c('MigrationRate', 'SplitTime', 'power')
 migr_fst_ss$power <- as.numeric(migr_fst_ss$power)
 migr_fst_ss$MigrationRate <- as.numeric(migr_fst_ss$MigrationRate)
-plot_title = 'SNP FST - Migration - HN (s=0.01)'
-gg_migr_fst_ss <- heatmap_power_gg(migr_fst_ss, 'MigrationRate', 'SplitTime', 'power', plot_title)
+plot_title = expression(italic(F[ST - MaxSNP])~" - Migration - High"~ italic(N[e])~" (s=0.01)")
+plot_name = 'migr_hn_ss_snp.jpg'
+gg_migr_fst_ss <- heatmap_power_gg(migr_fst_ss, 'MigrationRate', 'SplitTime', 'power', plot_title, "Migration Rate", "Split Time (coalescent units)", plot_name)
 gg_migr_fst_ss
 
 
@@ -237,8 +257,9 @@ migr_fst_ss <- data.frame(migr_fst_ss)
 names(migr_fst_ss) <- c('MigrationRate', 'SplitTime', 'power')
 migr_fst_ss$power <- as.numeric(migr_fst_ss$power)
 migr_fst_ss$MigrationRate <- as.numeric(migr_fst_ss$MigrationRate)
-plot_title = 'Window FST - Migration - HN (s=0.01)'
-gg_migr_fst_ss <- heatmap_power_gg(migr_fst_ss, 'MigrationRate', 'SplitTime', 'power', plot_title)
+plot_title = expression(italic(F[ST - Window])~" - Migration - High"~ italic(N[e])~" (s=0.01)")
+plot_name = 'migr_hn_ss_win.jpg'
+gg_migr_fst_ss <- heatmap_power_gg(migr_fst_ss, 'MigrationRate', 'SplitTime', 'power', plot_title, "Migration Rate", "Split Time (coalescent units)", plot_name)
 gg_migr_fst_ss
 
 # CHI
@@ -254,8 +275,9 @@ migr_chi_ss <- data.frame(migr_chi_ss)
 names(migr_chi_ss) <- c('MigrationRate', 'SplitTime', 'power')
 migr_chi_ss$power <- as.numeric(migr_chi_ss$power)
 migr_chi_ss$MigrationRate <- as.numeric(migr_chi_ss$MigrationRate)
-plot_title = 'CHI - Migration - HN (s=0.01)'
-gg_migr_chi_ss <- heatmap_power_gg(migr_chi_ss, 'MigrationRate', 'SplitTime', 'power', plot_title)
+plot_title = expression(italic("\u03A7"[MD])~" -Migration - High"~ italic(N[e])~" (s=0.01)")
+plot_name = 'migr_hn_ss_chi.jpg'
+gg_migr_chi_ss <- heatmap_power_gg(migr_chi_ss, 'MigrationRate', 'SplitTime', 'power', plot_title, "Migration Rate", "Split Time (coalescent units)", plot_name)
 gg_migr_chi_ss
 
 
@@ -273,9 +295,9 @@ migr_fst_ws <- data.frame(migr_fst_ws)
 names(migr_fst_ws) <- c('MigrationRate', 'SplitTime', 'power')
 migr_fst_ws$power <- as.numeric(migr_fst_ws$power)
 migr_fst_ws$MigrationRate <- as.numeric(migr_fst_ws$MigrationRate)
-
-plot_title = 'SNP FST - Migration - HN (s=0.001)'
-gg_migr_fst_ws <- heatmap_power_gg(migr_fst_ws, 'MigrationRate', 'SplitTime', 'power', plot_title)
+plot_title = expression(italic(F[ST - MaxSNP])~" - Migration - High"~ italic(N[e])~" (s=0.001)")
+plot_name = 'migr_hn_ws_snp.jpg'
+gg_migr_fst_ws <- heatmap_power_gg(migr_fst_ws, 'MigrationRate', 'SplitTime', 'power', plot_title, "Migration Rate", "Split Time (coalescent units)", plot_name)
 gg_migr_fst_ws
 
 # window
@@ -291,9 +313,9 @@ migr_fst_ws <- data.frame(migr_fst_ws)
 names(migr_fst_ws) <- c('MigrationRate', 'SplitTime', 'power')
 migr_fst_ws$power <- as.numeric(migr_fst_ws$power)
 migr_fst_ws$MigrationRate <- as.numeric(migr_fst_ws$MigrationRate)
-
-plot_title = 'Window FST - Migration - HN (s=0.001)'
-gg_migr_fst_ws <- heatmap_power_gg(migr_fst_ws, 'MigrationRate', 'SplitTime', 'power', plot_title)
+plot_title = expression(italic(F[ST - Window])~" - Migration - High"~ italic(N[e])~" (s=0.001)")
+plot_name = 'migr_hn_ws_win.jpg'
+gg_migr_fst_ws <- heatmap_power_gg(migr_fst_ws, 'MigrationRate', 'SplitTime', 'power', plot_title, "Migration Rate", "Split Time (coalescent units)", plot_name)
 gg_migr_fst_ws
 
 # CHI
@@ -309,9 +331,9 @@ migr_chi_ws <- data.frame(migr_chi_ws)
 names(migr_chi_ws) <- c('MigrationRate', 'SplitTime', 'power')
 migr_chi_ws$power <- as.numeric(migr_chi_ws$power)
 migr_chi_ws$MigrationRate <- as.numeric(migr_chi_ws$MigrationRate)
-
-plot_title = 'CHI - Migration - HN (s=0.001)'
-gg_migr_chi_ws <- heatmap_power_gg(migr_chi_ws, 'MigrationRate', 'SplitTime', 'power', plot_title)
+plot_title = expression(italic("\u03A7"[MD])~" -Migration - High"~ italic(N[e])~" (s=0.001)")
+plot_name = 'migr_hn_ws_chi.jpg'
+gg_migr_chi_ws <- heatmap_power_gg(migr_chi_ws, 'MigrationRate', 'SplitTime', 'power', plot_title, "Migration Rate", "Split Time (coalescent units)", plot_name)
 gg_migr_chi_ws
 
 ### LN
@@ -328,9 +350,9 @@ migr_fst_ln <- data.frame(migr_fst_ln)
 names(migr_fst_ln) <- c('MigrationRate', 'SplitTime', 'power')
 migr_fst_ln$power <- as.numeric(migr_fst_ln$power)
 migr_fst_ln$MigrationRate <- as.numeric(migr_fst_ln$MigrationRate)
-
-plot_title = 'SNP FST - Migration - LN (s=0.01)'
-gg_migr_fst_ln <- heatmap_power_gg(migr_fst_ln, 'MigrationRate', 'SplitTime', 'power', plot_title)
+plot_title = expression(italic(F[ST - MaxSNP])~" - Migration - Low"~ italic(N[e])~" (s=0.01)")
+plot_name = 'migr_ln_snp.jpg'
+gg_migr_fst_ln <- heatmap_power_gg(migr_fst_ln, 'MigrationRate', 'SplitTime', 'power', plot_title, "Migration Rate", "Split Time (coalescent units)", plot_name)
 gg_migr_fst_ln
 
 # Window
@@ -346,9 +368,9 @@ migr_fst_ln <- data.frame(migr_fst_ln)
 names(migr_fst_ln) <- c('MigrationRate', 'SplitTime', 'power')
 migr_fst_ln$power <- as.numeric(migr_fst_ln$power)
 migr_fst_ln$MigrationRate <- as.numeric(migr_fst_ln$MigrationRate)
-
-plot_title = 'Window FST - Migration - LN (s=0.01)'
-gg_migr_fst_ln <- heatmap_power_gg(migr_fst_ln, 'MigrationRate', 'SplitTime', 'power', plot_title)
+plot_title = expression(italic(F[ST - Window])~" - Migration - Low"~ italic(N[e])~" (s=0.01)")
+plot_name = 'migr_ln_win.jpg'
+gg_migr_fst_ln <- heatmap_power_gg(migr_fst_ln, 'MigrationRate', 'SplitTime', 'power', plot_title, "Migration Rate", "Split Time (coalescent units)", plot_name)
 gg_migr_fst_ln
 
 # CHI
@@ -364,7 +386,7 @@ migr_chi_ln <- data.frame(migr_chi_ln)
 names(migr_chi_ln) <- c('MigrationRate', 'SplitTime', 'power')
 migr_chi_ln$power <- as.numeric(migr_chi_ln$power)
 migr_chi_ln$MigrationRate <- as.numeric(migr_chi_ln$MigrationRate)
-
-plot_title = 'CHI - Migration - LN (s=0.01)'
-gg_migr_chi_ln <- heatmap_power_gg(migr_chi_ln, 'MigrationRate', 'SplitTime', 'power', plot_title)
+plot_title = expression(italic("\u03A7"[MD])~" -Migration - Low"~ italic(N[e])~" (s=0.01)")
+plot_name = 'migr_ln_chi.jpg'
+gg_migr_chi_ln <- heatmap_power_gg(migr_chi_ln, 'MigrationRate', 'SplitTime', 'power', plot_title, "Migration Rate", "Split Time (coalescent units)", plot_name)
 gg_migr_chi_ln
